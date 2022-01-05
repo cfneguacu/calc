52c7fb049223be38688bb2dd17ba444c6022a2af
Claudio <cfneguacu@uni9.edu.br>
2022-01-04 23:04:58 -0300

    Refatoração de codigo

--- calc/src/calc/Calc.java
+++ calc/src/calc/Calc.java
@@ -1,123 +1,109 @@
 package calc;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calc {
 	
-    public static void main(String [] args) { 
-   
-    int opcao=0;
-  
-    Scanner teclado = new Scanner(System.in);
-    
-    do {
+    public static void main(String [] args) {
+		menu();
+	}
 
-    System.out.println("-Seja bem-vindo(a) a sua calculadora-");
-    System.out.println("Escolha uma o por favor!!");
-    System.out.println("1. Soma");  
-    System.out.println("2. Subtracao");  
-    System.out.println("3. Multiplicacao");  
-    System.out.println("4. Divisao");  
-    System.out.println("0. Sair");  
-    System.out.print("Operação: "); 
-   
-    
-    opcao = teclado.nextInt();  
-    
-    if(opcao > 4) {
-    	System.out.print("Opcao Errada\n");
-    	main(null);
-    }else if (opcao == 0) {
-    	System.exit(0);
-    }
-  
-  	Soma(opcao);
-  
-    }while(opcao !=0);
-    
-    }
+	public static void menu(){
 
-    public static void Soma(int opcao) {
-    	
-    	
+			int opcao;
+			Scanner teclado = new Scanner(System.in);
 
-    	float valor1 = 0;
-    	boolean check = false;
-    	
-    	do {
+			do {
+
+				System.out.println("-Seja bem-vindo(a) a sua calculadora-");
+				System.out.println("Escolha uma opÃ§Ã£o por favor!!");
+				System.out.println("1. Soma");
+				System.out.println("2. SubtraÃ§Ã£o");
+				System.out.println("3. MultiplicaÃ§Ã£o");
+				System.out.println("4. DivisÃ£o");
+				System.out.println("0. Sair");
+				System.out.print("OperaÃ§Ã£o: ");
+
+
+				opcao = teclado.nextInt();
+
+				if(opcao > 4) {
+					System.out.print("Opcao Errada\n");
+					menu();
+				}else if(opcao == 0){
+					System.exit(0);
+				}
+
+				numeros(opcao);
+
+			}while(opcao !=0);
+
+
+	}
+
+    public static void numeros(int opcao) {
+
+    	String valor1;
+		String valor2;
+
+		Scanner teclado = new Scanner(System.in);
+
+		do {
     		
-    		Scanner teclado = new Scanner(System.in);
-    		
-        	try {
-    			System.out.print("Digite o primeiro valor:");
-    			
-    			valor1 = teclado.nextFloat();
-    			check = true;
-    			
-    		    
-    	 }catch(InputMismatchException ex) {
-    			
-    		    check = false; 
-    			System.out.println("Vamos começar denovo, digite apenas números teste");
-    			
-    	 }
-        	
-    	 }while(check==false);
-       
-    	//check = false;
-    	float valor2 = 0;
+			System.out.print("Digite o primeiro valor:");
+			valor1 = teclado.nextLine();
 
-        do{	
-        	
-        	Scanner teclado = new Scanner(System.in);
-        	
-            try {
-            		
-            	
-        			System.out.print("Digite o segundo valor:");
-        			
-        			valor2 = teclado.nextFloat();
-        			check = true;
-        		    
-        	 }catch(InputMismatchException ex) {
-        			
-        		    check = false; 
-        			System.out.println("Vamos começar denovo, digite apenas números teste");
-        			
-        	 }
-            	
-         }while(check==false);
+		}while(!checaNumero(valor1));
+
+        do{
+
+			System.out.print("Digite o segundo valor:");
+			valor2 = teclado.nextLine();
+
+         }while(!checaNumero(valor2));
         
         float resultado;
         
         if(opcao == 1) {
         	
-            resultado = valor1 + valor2;
+            resultado = Float.parseFloat(valor1) + Float.parseFloat(valor2);
         	System.out.print(resultado+"\n");
 
         }else if(opcao == 2) {
         	
-            resultado = valor1 - valor2;
+            resultado = Float.parseFloat(valor1) - Float.parseFloat(valor2);
         	System.out.print(resultado+"\n");
 
         }else if(opcao == 3) {
         	
-        	resultado = valor1 * valor2;
+        	resultado = Float.parseFloat(valor1) * Float.parseFloat(valor2);
         	System.out.print(resultado+"\n");
         	
         }else if(opcao == 4) {
-        	
-        	if(valor2 != 0) {
-        		
-        	resultado= valor1 / valor2;
+
+        	if(Float.parseFloat(valor2) != 0) {
+
+        	resultado= Float.parseFloat(valor1) / Float.parseFloat(valor2);
         	System.out.print(resultado+"\n");
-        	
+
         	}else {
-        		
-        	System.out.print("Divisão por 0\n");
-        	
+
+        	System.out.print("Divisï¿½o por 0\n");
+
         	}
         }
     }
+
+	public static boolean checaNumero(String valor) {
+
+	boolean check;
+
+	if(valor.matches("[0-9]")){
+		check = true;
+	}else{
+		System.out.println("Tente novamente");
+		check = false;
+	}
+		return check;
+	}
 }
